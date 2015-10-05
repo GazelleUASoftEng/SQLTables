a837a4ccac55a506726334a85303ba0d1677b6a9
ThyRex3 <kathydai@email.uark.edu>
2015-10-05 18:23:02 -0500

    Update EmployeeTable.sql
    Updated Employee_ID from integer to String

--- EmployeeTable.sql
+++ EmployeeTable.sql
@@ -2,7 +2,7 @@ CREATE TABLE Employee (
 	Record_ID uuid PRIMARY KEY,
 	First_Name varchar(50) NOT NULL,
 	Last_Name varchar(50) NOT NULL,
-	Employee_ID int NOT NULL,
+	Employee_ID String NOT NULL,
 	Active boolean DEFAULT FALSE,
 	Classification varchar(25) check (Classification in ('general manager', 'shift manager', 'cashier')),
 	Manager uuid,
