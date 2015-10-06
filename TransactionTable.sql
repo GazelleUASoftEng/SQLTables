52fe5dd1ccfa6ba973780e0a944fd963988fc49e
ThyRex3 <kathydai@email.uark.edu>
2015-10-06 18:53:47 -0500

    Update TransactionTable.sql

--- TransactionTable.sql
+++ TransactionTable.sql
@@ -1,19 +1,22 @@
-CREATE TABLE transaction (
-		record_id null NOT NULL,
-		cashier_id null NOT NULL,
-		amount NUMERIC(5 , 2) NOT NULL,
-		transactiontype VARCHAR(25),
-		parent_id null,
-		time_stamp TIMESTAMP DEFAULT now() NOT NULL
-	);
-
-CREATE UNIQUE INDEX transaction_pkey ON transaction (record_id ASC);
-
-ALTER TABLE transaction ADD CONSTRAINT transaction_pkey PRIMARY KEY (record_id);
-
-ALTER TABLE transaction ADD CONSTRAINT transaction_cashier_id_fkey FOREIGN KEY (cashier_id)
-	REFERENCES employee (record_id);
-
-ALTER TABLE transaction ADD CONSTRAINT transaction_parent_id_fkey FOREIGN KEY (parent_id)
-	REFERENCES transaction (record_id);
-
+CREATE TABLE transaction
+(
+  record_id uuid NOT NULL,
+  cashier_id uuid NOT NULL,
+  amount numeric(5,2) NOT NULL,
+  transactiontype character varying(25),
+  parent_id uuid,
+  time_stamp timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT transaction_pkey PRIMARY KEY (record_id),
+  CONSTRAINT transaction_cashier_id_fkey FOREIGN KEY (cashier_id)
+      REFERENCES employee (record_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT transaction_parent_id_fkey FOREIGN KEY (parent_id)
+      REFERENCES transaction (record_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT transaction_transactiontype_check CHECK (transactiontype::text = ANY (ARRAY['sale'::character varying, 'return'::character varying, 'void'::character varying]::text[]))
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE transaction
+  OWNER TO gazelle;
