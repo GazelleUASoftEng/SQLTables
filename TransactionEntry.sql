7a7ee21f8e582644bb597e0b3a6acdd8eb495e34
ThyRex3 <kathydai@email.uark.edu>
2015-10-06 18:54:10 -0500

    Update TransactionEntry.sql

--- TransactionEntry.sql
+++ TransactionEntry.sql
@@ -1,16 +1,18 @@
-CREATE TABLE transactionentry (
-		record_id null NOT NULL,
-		price FLOAT4,
-		quantity INT8,
-		transaction_id null NOT NULL,
-		createdon TIMESTAMP DEFAULT now() NOT NULL,
-		product_id null NOT NULL
-	);
-
-CREATE UNIQUE INDEX transactionentry_pkey ON transactionentry (record_id ASC);
-
-ALTER TABLE transactionentry ADD CONSTRAINT transactionentry_pkey PRIMARY KEY (record_id);
-
-ALTER TABLE transactionentry ADD CONSTRAINT fk_transentry_transaction FOREIGN KEY (transaction_id)
-	REFERENCES transaction (record_id);
-
+CREATE TABLE transactionentry
+(
+  record_id uuid NOT NULL,
+  price real,
+  quantity bigint,
+  transaction_id uuid NOT NULL,
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  product_id uuid NOT NULL,
+  CONSTRAINT transactionentry_pkey PRIMARY KEY (record_id),
+  CONSTRAINT fk_transentry_transaction FOREIGN KEY (transaction_id)
+      REFERENCES transaction (record_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE transactionentry
+  OWNER TO gazelle;
