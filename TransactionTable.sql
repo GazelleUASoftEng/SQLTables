95450ea15fcffd5026a65b4a5a55fbe1c49fe498
ThyRex3 <kathydai@email.uark.edu>
2015-10-06 18:26:51 -0500

    Update TransactionTable.sql
    Original Transaction Table BACKUP

--- TransactionTable.sql
+++ TransactionTable.sql
@@ -1,10 +1,19 @@
-CREATE TABLE Transaction ( 
-	Record_ID uuid PRIMARY KEY,
-	Cashier_ID uuid  NOT NULL,
-	Foreign Key (Cashier_ID) References Employee(Record_ID),
-	Amount decimal(5,2) NOT NULL,
-	TransactionType varchar(25) check (TransactionType in ('sale', 'return', 'void')),
-	Parent_ID uuid,
-	Foreign Key (Parent_ID) References Transaction (Record_ID),
-	Time_Stamp timestamp without time zone NOT NULL DEFAULT now()
-);
+CREATE TABLE transaction (
+		record_id null NOT NULL,
+		cashier_id null NOT NULL,
+		amount NUMERIC(5 , 2) NOT NULL,
+		transactiontype VARCHAR(25),
+		parent_id null,
+		time_stamp TIMESTAMP DEFAULT now() NOT NULL
+	);
+
+CREATE UNIQUE INDEX transaction_pkey ON transaction (record_id ASC);
+
+ALTER TABLE transaction ADD CONSTRAINT transaction_pkey PRIMARY KEY (record_id);
+
+ALTER TABLE transaction ADD CONSTRAINT transaction_cashier_id_fkey FOREIGN KEY (cashier_id)
+	REFERENCES employee (record_id);
+
+ALTER TABLE transaction ADD CONSTRAINT transaction_parent_id_fkey FOREIGN KEY (parent_id)
+	REFERENCES transaction (record_id);
+
