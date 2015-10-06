ba71a831d8fa2b1f2d1d69f8906a790e1fa7f3cf
ThyRex3 <kathydai@email.uark.edu>
2015-10-06 15:40:09 -0500

    Update EmployeeTable.sql
    Copied Script from pgAdmin III

--- EmployeeTable.sql
+++ EmployeeTable.sql
@@ -1,12 +1,24 @@
-CREATE TABLE Employee (
-	Record_ID uuid PRIMARY KEY,
-	First_Name varchar(50) NOT NULL,
-	Last_Name varchar(50) NOT NULL,
-	Employee_ID varchar(15) NOT NULL,
-	Active boolean DEFAULT FALSE,
-	Classification varchar(25) check (Classification in ('general manager', 'shift manager', 'cashier')),
-	Manager uuid,
-	FOREIGN KEY (Manager) REFERENCES Employee(Record_ID),
-	Password varchar(50) NOT NULL,
-	Time_Stamp timestamp without time zone NOT NULL DEFAULT now()
-	);
+-- Table: employee
+
+CREATE TABLE employee
+(
+  record_id uuid NOT NULL,
+  first_name character varying(50) NOT NULL,
+  last_name character varying(50) NOT NULL,
+  employee_id character varying(15) NOT NULL,
+  active boolean NOT NULL DEFAULT false,
+  classification character varying(25),
+  manager uuid,
+  password character varying(50) NOT NULL,
+  time_stamp timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT employee_pkey1 PRIMARY KEY (record_id),
+  CONSTRAINT employee_manager_fkey1 FOREIGN KEY (manager)
+      REFERENCES employee (record_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT employee_classification_check1 CHECK (classification::text = ANY (ARRAY['general manager'::character varying, 'shift manager'::character varying, 'cashier'::character varying]::text[]))
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE employee
+  OWNER TO gazelle;
