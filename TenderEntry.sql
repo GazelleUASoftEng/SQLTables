05a7bdd71d05ac25f59be3ab764bc5ad0dacf286
ThyRex3 <kathydai@email.uark.edu>
2015-11-01 22:14:51 -0600

    Fixed datatypes for Record_ID and PayType

--- TenderEntry.sql
+++ TenderEntry.sql
@@ -1,15 +1,17 @@
-CREATE TABLE tenderentry (
-		recordid INT4 NOT NULL,
-		amount FLOAT8 NOT NULL,
-		paytype CIDR(2147483647),
-		time TIMESTAMP,
-		transactionid null
-	);
-
-CREATE UNIQUE INDEX tenderentry_pkey ON tenderentry (recordid ASC);
-
-ALTER TABLE tenderentry ADD CONSTRAINT tenderentry_pkey PRIMARY KEY (recordid);
-
-ALTER TABLE tenderentry ADD CONSTRAINT tenderentry_transactionid_fkey FOREIGN KEY (transactionid)
-	REFERENCES transaction (record_id);
-
+CREATE TABLE tenderentry
+(
+  record_id uuid NOT NULL,
+  amount double precision NOT NULL,
+  paytype integer NOT NULL,
+  "time" timestamp without time zone NOT NULL,
+  transactionid uuid,
+  CONSTRAINT tenderentry_pkey PRIMARY KEY (record_id),
+  CONSTRAINT tenderentry_transactionid_fkey FOREIGN KEY (transactionid)
+      REFERENCES transaction (record_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE tenderentry
+  OWNER TO gazelle;
